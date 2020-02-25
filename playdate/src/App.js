695ac9d8615880b8b0b626bc2f2161831059e6fa
Avelynn Mitra <avelynnmitra@gmail.com>
2020-02-25 14:53:14 -0600

    footer business

--- playdate/src/App.js
+++ playdate/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="App-inner-container">
         
       </div>
+      <Footer />
     </div>
   );
 }
