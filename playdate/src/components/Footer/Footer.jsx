3c1a0563025dcb1c41cf075523c4d2793edf543d
Avelynn Mitra <avelynnmitra@gmail.com>
2020-02-25 14:37:26 -0600

    got my footer y'all

---
+++ playdate/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Footer.module.css';
+
+const Footer = (props) => {
+    return (
+        <footer className={styles.footer}>
+            <h1>Copyright playDate</h1>
+        </footer>
+    );
+}
+
+export default Footer;
