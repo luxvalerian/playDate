fcc06956e330e57780ad4cbaf9ebec4bf5b1a2b7
Avelynn Mitra <avelynnmitra@gmail.com>
2020-02-26 12:13:18 -0600

    add pages folder

---
+++ playdate/src/pages/Playdates/Playdates.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Playdates.module.css';
+
+const Playdates = (props) => {
+    return (
+        <main>
+            <h1>Playdates</h1>
+        </main>
+    );
+};
+
+export default Playdates;
