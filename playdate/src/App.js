79df554f12d435c7216525ff1bc7f2915935300c
Avelynn Mitra <avelynnmitra@gmail.com>
2020-02-26 12:08:46 -0600

    add page component and routes

--- playdate/src/App.js
+++ playdate/src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+// reusable components --presentational
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+
+//  page components
+import Home from './pages/Home/Home';
+import Playdates from './pages/Playdates/Playdates';
+
 import './App.css';
 
 function App() {
@@ -8,6 +16,14 @@ function App() {
     <div className="App-outer-container">
       <Navbar />
       <div className="App-inner-container">
+        <Switch>
+          <Route exact path="/" render={props =>
+            <Home />
+          }/>
+          <Route exact path="/playdates" render={props =>
+            <Playdates />
+          }/>
+        </Switch>
         
       </div>
       <Footer />
