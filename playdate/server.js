2434f377b141ec380039f1f1ce3261b3e0f96cea
Avelynn Mitra <avelynnmitra@gmail.com>
2020-02-29 11:56:18 -0600

    installed backend dep

---
+++ playdate/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const logger = require('morgan');
+const serveFavicon = require('serve-favicon');
+const path = require('path');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(serveFavicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+                        // from playdate/build/index.html
+})
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express is listening on port: ${port}`);
+})
