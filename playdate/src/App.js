3929d629c025d7b8ffed540a6fc8ee3cff516d66
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-03 15:44:44 -0600

    Redirect user to login

--- playdate/src/App.js
+++ playdate/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // reusable components --presentational
 import Navbar from './components/Navbar/Navbar';
@@ -12,31 +12,55 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
 import './App.css';
+import userService from './utils/userService';
 
-function App() {
-  return (
-    <div className="App-outer-container">
-      <Navbar />
-      <div className="App-inner-container">
-        <Switch>
-          <Route exact path="/" render={props =>
-            <Home />
-          }/>
-          <Route exact path="/playdates" render={props =>
-            <Playdates />
-          }/>
-          <Route exact path="/login" render={props => 
-            <Login /> //page component login
-          }/>
-          <Route exact path="/signup" render={props => //route for signup
-            <Signup />
-          }/>
-        </Switch>
-        
+class App extends Component {
+
+  state = {
+    user: userService.getUser()
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser()})
+  }
+
+  handleLogout = () => {
+    // we need to call userService.logout();
+    userService.logout();
+    // we need to set the user property on state to null
+    this.setState({ user: null});
+  }
+
+  render() {
+      return (
+        <div className="App-outer-container">
+        <Navbar handleLogout={this.handleLogout} />
+        <div className="App-inner-container">
+          <Switch>
+            <Route exact path="/" render={props =>
+              <Home />
+            }/>
+            <Route exact path="/playdates" render={props =>
+            userService.getUser()
+            ? <Playdates />
+            : <Redirect to="/login" />
+            }/>
+            <Route exact path="/login" render={props => 
+              <Login /> //page component login
+            }/>
+            <Route exact path="/signup" render={props => //route for signup
+              <Signup 
+              {...props} 
+              handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            }/>
+          </Switch>
+          
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
