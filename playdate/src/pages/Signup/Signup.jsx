302beb50cd515e799e74b65b226ff0fabfe0d9af
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-03 15:48:04 -0600

    add users to state

--- playdate/src/pages/Signup/Signup.jsx
+++ playdate/src/pages/Signup/Signup.jsx
@@ -5,7 +5,10 @@ const Signup = (props) => {
     return (
         <main>
             <h1>Signup</h1>
-            <SignupForm />
+            <SignupForm 
+                {...props} 
+                handleSignupOrLogin={props.handleSignupOrLogin}
+            />
         </main>
     );
 };
