3fb08f51e7609c92c9b2a89b79cade33b8e5267c
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-03 15:40:48 -0600

    connect local server bc sbx wifi :/

--- playdate/config/database.js
+++ playdate/config/database.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
-mongoose.connect(process.env.DATABASE_URL, {
+
+mongoose.connect('mongodb://localhost/playdate'/*process.env.DATABASE_URL*/, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-});
+}).catch(err => console.log('error: ', err))
 
 db.on('connected', function() {
     console.log(`MongoDB connected on ${db.host}:${db.port}`);
