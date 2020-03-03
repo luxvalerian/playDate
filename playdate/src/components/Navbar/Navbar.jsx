f9daa96b846bf405626df43944d1612cf3153233
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-03 15:46:01 -0600

    conditional UI upon logout/in

--- playdate/src/components/Navbar/Navbar.jsx
+++ playdate/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+
+import userService from '../../utils/userService';
 
 import styles from './Navbar.module.css';
 
 const Navbar = (props) => {
+    const conditionalUI = userService.getUser() 
+    ? <Fragment>
+            <li>
+                <Link to="/playdates">Playdates</Link>
+            </li>
+            <li>
+                <Link to="" onClick={props.handleLogout}>Logout</Link>
+            </li> 
+      </Fragment>
+    :    
+      <Fragment>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/signup">Sign Up!</Link>
+            </li>
+        </Fragment>
+
     return (
         <nav className={styles.navbar}>
             <Link to="/">
                 <h1>playDate</h1>
             </Link>
             <ul>
-                <li>
-                    <Link to="/playdates">Playdates</Link>
-                </li> 
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/signup">Sign Up!</Link>
-                </li>
+                { conditionalUI }
             </ul>
         </nav>
     );
