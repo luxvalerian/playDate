cfe4dc6e25905f5bd0b5b78bf36f044e1d5bef2f
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-05 12:14:32 -0600

    done on the client side

--- playdate/src/utils/userService.js
+++ playdate/src/utils/userService.js
@@ -2,6 +2,29 @@ import tokenService from './tokenService';
 
 const BASE_URL = '/api/users/';
 
+// this is where we contact the server via AJAX, which contacts the database
+// looks for email that matches and compares with password in database
+// if matching, token is created and sent back to form (tokenService)
+// if not, send back a 400 saying 'bad request' or 'email doesn't exist'
+
+// set up fetch request to send information to server:
+
+function login(creds) {
+    return fetch(BASE_URL + 'login', {
+        method: 'POST',
+        headers: new Headers({ 'Content-type': 'Application/json' }),
+        body: JSON.stringify(creds)
+    })
+    .then(response => {
+        if(response.ok) {
+            return response.json(); 
+        } else {
+            throw new Error('Bad Credentials');
+        }
+    })
+    .then(({ token }) => tokenService.setToken(token))
+}
+
 
 function logout() {
     tokenService.removeToken();
@@ -31,7 +54,8 @@ function signup(user) {
 export default {
     signup,
     getUser,
-    logout
+    logout,
+    login
 }
 
 // TODO - make a login and logout function and export 
