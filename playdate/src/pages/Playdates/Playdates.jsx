c3bd0df6800dffad3f799c9d1a7f2f3d55e65c2e
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-12 16:31:38 -0500

    import PlaydateForm add props

--- playdate/src/pages/Playdates/Playdates.jsx
+++ playdate/src/pages/Playdates/Playdates.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import PlaydateForm from '../../components/PlaydateForm/PlaydateForm';
+
+
+
 const Playdates = (props) => {
     return (
         <main>
             <h1>Playdates</h1>
+            <PlaydateForm {...props} />
         </main>
     );
 };
