fd88c3ef6fd4e6c29342b22ffad6d67e8c6689a8
Avelynn Mitra <avelynnmitra@gmail.com>
2020-04-28 16:49:10 -0500

    router get featured

--- playdate/routes/api/playdates.js
+++ playdate/routes/api/playdates.js
@@ -3,6 +3,7 @@ const playdateCtrl = require('../../controllers/playdates');
 
 router.post('/', playdateCtrl.create);
 router.get('/', playdateCtrl.index);
+router.get('/featured', playdateCtrl.getFeatured);
 
 
 module.exports = router;
