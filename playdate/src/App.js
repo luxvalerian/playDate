cd05884a29f083a6d623c14aa90e5212170a7d78
Avelynn Mitra <avelynnmitra@gmail.com>
2020-04-28 16:49:43 -0500

    add playdates and featured to state

--- playdate/src/App.js
+++ playdate/src/App.js
@@ -13,22 +13,44 @@ import Signup from './pages/Signup/Signup';
 
 import './App.css';
 import userService from './utils/userService';
+import playdateService from './utils/playdateService';
 
 class App extends Component {
 
   state = {
-    user: userService.getUser()
+    user: userService.getUser(),
+    playdates: [],
+    featuredPlaydates: []
   }
 
   handleSignupOrLogin = () => {
-    this.setState({ user: userService.getUser()})
+    this.setState({ user: userService.getUser()}, () => {
+      this.handleGetPlaydates();
+    })
   }
 
   handleLogout = () => {
     // we need to call userService.logout();
     userService.logout();
     // we need to set the user property on state to null
-    this.setState({ user: null});
+    this.setState({ user: null, playdates: [] });
+  }
+
+  handleGetPlaydates = async () => {
+    if(userService.getUser()) {
+      const { playdates } = await playdateService.index();
+      this.setState({ playdates });
+    }
+  }
+
+  handleGetFeaturedPlaydates = async () => {
+    const { featuredPlaydates } = await playdateService.getFeatured();
+    this.setState({ featuredPlaydates });
+  }
+
+  componentDidMount () {
+    this.handleGetFeaturedPlaydates();
+    this.handleGetPlaydates();
   }
 
   render() {
@@ -38,11 +60,15 @@ class App extends Component {
         <div className="App-inner-container">
           <Switch>
             <Route exact path="/" render={props =>
-              <Home />
+              <Home featuredPlaydates={this.state.featuredPlaydates}/>
             }/>
             <Route exact path="/playdates" render={props =>
             userService.getUser()
-            ? <Playdates {...props}/>
+            ? <Playdates 
+            {...props}
+            handleGetPlaydates={this.handleGetPlaydates}
+            playdates={this.state.playdates}
+            />
             : <Redirect to="/login" />
             }/>
             <Route exact path="/login" render={props => 
