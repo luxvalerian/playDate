2c883118a7a1a349defd986adabb43f064259e85
Avelynn Mitra <avelynnmitra@gmail.com>
2020-04-28 16:50:40 -0500

    add in/visible form feature

--- playdate/src/pages/Playdates/Playdates.jsx
+++ playdate/src/pages/Playdates/Playdates.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PlaydateForm from '../../components/PlaydateForm/PlaydateForm';
 
 
 
 const Playdates = (props) => {
+    const [ formVisible, setVisibility ] = useState(false);
     return (
         <main>
             <h1>Playdates</h1>
+            <button onClick={() => setVisibility(!formVisible)}>
+                {formVisible ? 'Hide Form' : 'Show Form'}
+            </button>
+            {
+                formVisible  &&
             <PlaydateForm {...props} />
+            }
+            {
+                props.playdates.map(({title, _id, location, addedBy}) => (
+                    <section key={_id}>
+                        <h1>{title}</h1>
+                        <p>Location: {location}</p>
+                        <small>Added By: {addedBy.name}</small>
+                    </section>
+                ))
+            }
         </main>
     );
 };
