c1f5ff01698aa788edb7fb01bb5d46cfef636e86
Avelynn Mitra <avelynnmitra@gmail.com>
2020-04-28 14:18:19 -0500

    edit line 4 mongoose.connect

--- playdate/config/database.js
+++ playdate/config/database.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
-
-mongoose.connect('mongodb://localhost/playdate'/*process.env.DATABASE_URL*/, {
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/playdate', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 }).catch(err => console.log('error: ', err))
-
 db.on('connected', function() {
     console.log(`MongoDB connected on ${db.host}:${db.port}`);
 });
